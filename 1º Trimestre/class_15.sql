684a466a373e6e427ff3a0439227cd1d36c59501
emiborioni <emiborionicab@gmail.com>
2018-08-12 22:58:05 -0300

    update class_15.sql excersice 4

--- 1º Trimestre/class_15.sql
+++ 1º Trimestre/class_15.sql
@@ -1,4 +1,5 @@
 # class 15 
+#1
 CREATE OR REPLACE VIEW list_of_customer AS 
   SELECT customer_id, CONCAT(first_name ," ", last_name) AS full_name , address.address, address.postal_code, address.phone, city.city, country.country, store_id
   FROM customer 
@@ -7,7 +8,7 @@ CREATE OR REPLACE VIEW list_of_customer AS
   INNER join country  using (country_id);
  
  select * from list_of_customer;
-
+#2
 #Create a view named film_details, it should contain the following columns:
 #film id,  title, description,  category,  price,  length,  rating, actors  - as a string of all the actors separated by comma. Hint use GROUP_CONCAT
 
@@ -22,7 +23,7 @@ CREATE OR REPLACE VIEW list_of_customer AS
  group by film_id, title, description,`length`, rating, category.name,replacement_cost
  
  select * from film_details ;
-
+#3
  #Create view sales_by_film_category, it should return 'category' and 'total_rental' columns. SUM(payment.amount)
  CREATE OR REPLACE VIEW sales_by_film_category AS
  select name, SUM(payment.amount)
@@ -35,3 +36,16 @@ CREATE OR REPLACE VIEW list_of_customer AS
  group by name
  
  select * from sales_by_film_category ; 
+
+#4
+#Create a view called actor_information where it should return, actor id, first name, last name and the amount of films he/she acted on.
+
+ CREATE OR REPLACE VIEW actor_information AS
+ select actor_id, first_name, last_name, COUNT(film_actor.actor_id)
+ from actor
+ INNER join film_actor USING (actor_id)
+ where (film_actor.actor_id = actor.actor_id)
+ group by actor_id
+ ; 
+ 
+ select * from actor_information; 

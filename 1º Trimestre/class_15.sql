a69251e7c1d49f6476c3f8711062f77e92580518
emiborioni <emiborionicab@gmail.com>
2018-08-18 16:31:09 -0300

    update class15.sql from martor

--- 1º Trimestre/class_15.sql
+++ 1º Trimestre/class_15.sql
@@ -57,3 +57,40 @@ CREATE OR REPLACE VIEW list_of_customer AS
 # Lo que realiza la query es extraer de la tabla actor los siguientes parametros (actor_id, first_name, last_name)y de la tabla film_actor, se coloca un contador del campo actor_id cuando el id 
 # coincida con el de la tabla actor en el campo actor_id, pero como se encuentra en una tabla distinta necesitamos realizar un inner join que nos vincule dichas tablas, 
 # luego de esto para que ordene en cuantas peliculas actuo cada uno realizamos un group by el cual agrupara todos los resultados que tengan el mismo campo actor_id. 
+
+
+#EJERCICIO COPIADO A MONTANE 
+
+#5
+#we select id, firstname, lastname  del actor 
+SELECT a.actor_id AS actor_id, a.first_name AS first_name, a.last_name AS last_name,
+	# then we concatenate the category name and 
+    group_concat(
+    DISTINCT concat(
+    	c.name, ': ', (
+    	# the movies that have that category and the actor in it
+    	SELECT group_concat(f.title ORDER BY f.title ASC separator ', ')
+                FROM sakila.film f 
+                # here we join the tables
+                	JOIN sakila.film_category fc ON(f.film_id = fc.film_id)
+                    JOIN sakila.film_actor fa ON(f.film_id = fa.film_id)
+                #and check that the actor and the category are the saem
+                WHERE fc.category_id = c.category_id
+                    AND fa.actor_id = a.actor_id)
+        )
+    	ORDER BY c.name ASC SEPARATOR '; ') AS film_info 
+    # we add all the tables
+    FROM sakila.actor a
+    	LEFT JOIN sakila.film_actor fa ON(a.actor_id = fa.actor_id)
+        LEFT JOIN sakila.film_category fc ON(fa.film_id = fc.film_id)
+    	LEFT JOIN sakila.category c ON(fc.category_id = c.category_id)
+# group by actors so that we can group concat
+GROUP BY a.actor_id, a.first_name, a.last_name
+
+SELECT * FROM actor_info
+
+#6
+#Materialized views are views that write the result of the query they run in a temporary table.
+#this allows the search to be quicker but if data changes and the view is not refreshed the data will
+#be out of date. This is used for data that doesn't change and to take statistics of the day before. They exist in 
+# Oracle - PostgreSQL - SQL Server - Apache Kafkaand and Apache Spark -  Sybase SQL - IBM DB2 - Microsoft SQL Server 

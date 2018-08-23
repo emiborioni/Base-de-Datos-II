513ef47167b2fe05c0955103428ed81d027b5691
emiborioni <emiborionicab@gmail.com>
2018-08-23 10:23:02 -0300

    update class 14.sql excersice 5 y 6

--- 1º Trimestre/class_14.sql
+++ 1º Trimestre/class_14.sql
@@ -48,3 +48,48 @@ INNER JOIN inventoryz USING (inventory_id)
 INNER JOIN film USING (film_id)
 INNER JOIN customer USING (customer_id)
 where MONTH(return_date) in ('5','6');
+
+
+#5
+CAST:
+Convert a value from one datatype to another datatype.
+eg: SELECT CAST("2017-08-29" AS DATE); 
+
+CONVERT:
+Convert a value from one datatype to another datatype.
+eg:  SELECT CONVERT("2017-08-29", DATE); 
+
+CAST and CONVERT do the same thing, except that CONVERT allows more options, such as changing character set with USING. CAST uses 'AS' and CONVERT a comma.
+
+EXAMPLES BASED ON SAKILA DB
+SELECT CAST(rental_date AS CHAR) from rental WHERE rental_id=80;
+SELECT CONVERT(amount ,SIGNED) from payment WHERE payment_id=80;
+
+#6
+|NVL FUNCTION|
+
+The NVL() function is available in Oracle, and not in MySQL or SQL Server. 
+This function is used to replace NULL value with another value. It is similar to the IFNULL Function in MySQL.
+
+|IFNULL FUNCTION|
+
+So NVL() in MYSQL is IFNULL():
+SELECT title, CONCAT(first_name, " ", last_name) as Nombre, 
+IFNULL(return_date, "NOT RETURNED YET")
+FROM rental
+INNER JOIN inventory USING(inventory_id)
+INNER JOIN film USING(film_id)
+INNER JOIN customer USING(customer_id);
+
+If the first expression isn't null, it returns itself. Otherwise, returns the second one.
+In this example if rental_date is null then it'll be selected with the text "NOT RETURNED YET".
+
+|ISNULL FUNCTION|
+Test whether an expression is NULL
+SELECT ISNULL(return_date) from rental;
+In this case it returns 1(true) or 0(false)
+
+|COALESCE FUNCTION|
+Return the first non-null expression in a list
+SELECT COALESCE(email,last_name) from customer;
+In this case if a customer does not have an email it'll return the last_name.							
